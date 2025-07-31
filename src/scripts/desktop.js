90ae6d17fa2b0302979fd34b6a56e16e7bebc9f0
fishbread <123308170+fish-bread@users.noreply.github.com>
2025-07-31 13:51:25 +0800

    Delete src directory

--- src/scripts/desktop.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from "vue"
-import DesktopApp from "@/windows/desktop/DesktopApp.vue"
-import { getSharedPinia } from "@/stores"
-
-const desktopApp = createApp(DesktopApp)
-desktopApp.use(getSharedPinia())
-desktopApp.mount("#desktopApp")
--- src/scripts/in-game.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from "vue"
-import InGameApp from "@/windows/in-game/InGameApp.vue"
-import { getSharedPinia } from "@/stores"
-
-const inGameApp = createApp(InGameApp)
-inGameApp.use(getSharedPinia())
-inGameApp.mount("#inGameApp")
